a2aa751c945d2b05ef866fe47ec87075e405a385
itumeleng <120252503+itumeleng-itu@users.noreply.github.com>
2026-02-24 11:41:41 +0000

    fix: fix color stlyling for the navigation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,22 +6,21 @@ const xefagLogo = require('@/assets/images/xefag.png');
 export default function Nav() {
   return (
     <View className="flex-row gap-5 items-center justify-between mx-5 mt-3">
-        <Pressable className="w-14 h-14 items-center justify-center bg-white rounded-full active:bg-white">
+        <Pressable className="w-14 h-14 items-center justify-center bg-white rounded-full active:bg-black">
             <Entypo name="chevron-left" size={30} color="black"/>
         </Pressable>
         <View className="flex-row gap-2 items-center flex-1 ">
             <Image source={xefagLogo} style={{ width: 130, height: 110 }} />
         </View>
-        <Pressable className="w-14 h-14 items-center justify-center bg-black/40 rounded-full active:bg-white"
-              style={{ backgroundColor: 'rgb(240, 133, 11)' }}>
+        <Pressable className="w-14 h-14 items-center justify-center bg-orange-400 rounded-full active:bg-white">
             <Ionicons name="person-outline" size={24} color="white" />
         </Pressable>
-        <View className="relative w-14 h-14 items-center justify-center bg-[#e6e6d8] rounded-full">
+        <Pressable className="relative w-14 h-14 items-center justify-center bg-[#e6e6d8] rounded-full active:bg-orange-400">
             <Entypo name="shopping-basket" size={24} color="black" />
             <View className="absolute top-0 right-0 w-6 h-6 bg-black rounded-full items-center justify-center">
                 <Text className="text-white text-xs font-bold">3</Text>
             </View>
-        </View>
+        </Pressable>
     </View>
   );
 }
